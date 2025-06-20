bebbc20170a25fb17f3ff7c88bebe205e65de372
Sergey Philippov <quadratte@gmail.com>
2025-06-20 17:02:04 +0700

    Initial Commit

---
+++ SecondApp/SecondAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondAppApp.swift
+//  SecondApp
+//
+//  Created by Quadratte on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SecondAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
